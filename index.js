8ace16c401e84dd6613fdf09fa7cc29bb1295189
dfgsdsfg2gdgdf <gs1omat8483@hotmail.com>
2024-05-16 04:53:02 +0700

    aptos

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
-const countingSort = (arr) => {
-  const min = Math.min(...arr);
-  const max = Math.max(...arr);
-  const count = Array(max - min + 1).fill(0);
-  for (let num of arr) {
-    count[num - min]++;
-  }
-  let sortedIndex = 0;
-  for (let i = min; i <= max; i++) {
-    while (count[i - min] > 0) {
-      arr[sortedIndex++] = i;
-      count[i - min]--;
+function jump(nums) {
+  let jumps = 0;
+  let currentEnd = 0;
+  let farthest = 0;
+  for (let i = 0; i < nums.length - 1; i++) {
+    farthest = Math.max(farthest, i + nums[i]);
+    if (i === currentEnd) {
+      jumps++;
+      currentEnd = farthest;
     }
   }
-  return arr;
-};
+  return jumps;
+}
